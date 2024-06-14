6083eaadb9fda22514ddc66d04b9cb76641b5b1d
Aswin Reghu <136116460+PRE53RVER@users.noreply.github.com>
2024-06-14 22:37:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
@@ -16,8 +15,6 @@ secondaryBackgroundColor="#F0F2F6"
 textColor="#262730"
 font="sans serif"
 
-
-# This is the first API key input; no need to repeat it in the main function.
 st.sidebar.title("Menu:")
 api_key = st.sidebar.text_input("Enter your Google API Key:", type="password", key="api_key_input")
 
@@ -35,14 +32,19 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(text_chunks, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    try:
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+        vector_store.save_local("faiss_index")
+        st.success("Vector store created successfully.")
+    except Exception as e:
+        st.error(f"An error occurred while embedding documents: {e}")
+        st.stop()
 
-def get_conversational_chain():
+def get_conversational_chain(api_key):
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer, make the conversation more intractive, always greet befor answering\n\n
+    provided context just say, "answer is not available in the context", don't provide the wrong answer, make the conversation more intractive, always greet before answering\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
@@ -54,24 +56,26 @@ def get_conversational_chain():
     return chain
 
 def user_input(user_question, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-    chain = get_conversational_chain()
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-    return response
+    try:
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+        new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+        docs = new_db.similarity_search(user_question)
+        chain = get_conversational_chain(api_key)
+        response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+        return response
+    except Exception as e:
+        st.error(f"An error occurred during the user input processing: {e}")
+        return None
 
 def main():
     st.header("wizPDF")
-    # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
+
     help = """
-
-
 Follow these simple steps to interact with me:
 
-1. **Enter Your API Key**: You'll need a Google API key for the chatbot to access Google's Generative AI models. Obtain your API key https://makersuite.google.com/app/apikey.
+1. **Enter Your API Key**: You'll need a Google API key for the chatbot to access Google's Generative AI models. Obtain your API key [here](https://makersuite.google.com/app/apikey).
 
 2. **Upload Your Documents**: The system accepts multiple PDF files at once, analyzing the content to provide comprehensive insights.
 
@@ -80,38 +84,27 @@ Follow these simple steps to interact with me:
     with st.chat_message("assistant"):
         st.markdown(help)
 
-    # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
-    
 
-    
-
-
-    # React to user input
     if prompt := st.chat_input("Ask a Question from the PDF Files"):
-        # Display user message in chat message container
         st.chat_message("user").write(prompt)
-        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         response = user_input(prompt, api_key)
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.write(response['output_text'])
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response['output_text']})
-        
+        if response:
+            with st.chat_message("assistant"):
+                st.write(response['output_text'])
+            st.session_state.messages.append({"role": "assistant", "content": response['output_text']})
 
     with st.sidebar:
         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-        if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
+        if st.button("Submit & Process", key="process_button") and api_key:
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks, api_key)
-                st.success("Done")
 
 if __name__ == "__main__":
     main()
